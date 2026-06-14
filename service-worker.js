0110c7366077b7df4a5912cadd7c2ee33ffb7ec0
Simone Bennett <lol_gigglez@hotmail.com>
2026-06-14 00:46:26 -0400

    Add minimal service-worker.js to stop 404

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+﻿self.addEventListener("install", event => self.skipWaiting());
+self.addEventListener("activate", event => event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", event => {
+  // no caching; just let requests pass through
+});
